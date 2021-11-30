6fcd0dea174a9e5a4476f448fa67a3d89d4ebbee
RaheemWilson <wilsonzackery16@gmail.com>
2021-11-30 13:32:43 -0500

    Changed server methods

--- php/index.php
+++ php/index.php
@@ -10,58 +10,59 @@ $conn = initialiseDatabase();
 ?>
 
 <?php
-    if(isset($_GET["section"])){
-        $section = $_GET["section"];
-        include "form.php";
-    }   
- ?>
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+  if(isset($_GET["section"])){
+    $section = $_GET["section"];
+    include "form.php";
+  } 
+} elseif($_SERVER["REQUEST_METHOD"] == "POST"){
+  $post = json_decode(file_get_contents('php://input'), true);
 
+  if(isset($post)){
+    $cmd=$post['status'];
 
+    switch ($cmd) {
+      case "login":
+          $email = $post['email'];
+          $password = $post['password'];
+          $result = checkEntry($email, $password, $conn);
 
-<?php
-$post = json_decode(file_get_contents('php://input'), true);
-
-if(isset($post)){
-  $cmd=$post['status'];
-
-  switch ($cmd) {
-    case "login":
-        $email = $post['email'];
-        $password = $post['password'];
-        $result = checkEntry($email, $password, $conn);
-
+          if($result){
+            session_start();
+            $_SESSION["useremail"] = $email;
+            include "home.php";
+          }else{
+            echo "User not found";
+          }
+      break;
+      case "add-user":
+        $result = TRUE;
+        // $result = addUser($post)
         if($result){
-          session_start();
-          $_SESSION["useremail"] = $email;
-          include "home.php";
+          http_response_code(201);
+          echo json_encode(array("message" => "User was succesfully added"));
+          
         }else{
-          echo "User not found";
+          http_response_code(500);
+          echo json_encode(array("message" => "User not created"));
         }
-    break;
-    case "add-user":
-      $result = TRUE;
-      // $result = addUser($post)
-      if($result){
-        http_response_code(201);
-        echo json_encode(array("message" => "User was succesfully added"));
-        
-      }else{
-        http_response_code(500);
-        echo json_encode(array("message" => "User not created"));
-      }
-    break;
-    case "new-issue":
-      $result = TRUE;
-      // $result = addUser($post)
-      if($result){
-        http_response_code(201);
-        echo json_encode(array("message" => "Issue was succesfully added"));
-        
-      }else{
-        http_response_code(500);
-        echo json_encode(array("message" => "Issue not created"));
-      }
-    break;
+      break;
+      case "new-issue":
+        $result = TRUE;
+        // $result = addNewIssue($post)
+        if($result){
+          http_response_code(201);
+          echo json_encode(array("message" => "Issue was succesfully added"));
+          
+        }else{
+          http_response_code(500);
+          echo json_encode(array("message" => "Issue not created"));
+        }
+      break;
+    }
   }
 }
+      
+
+
 
